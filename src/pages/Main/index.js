6f5b34fff75d21d4f1d4c67162945e7ca783b58c
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-03-30 13:58:14 -0300

    api and stylizing Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,14 @@
-import React, { Component } from "react";
+import React, {
+  Component
+} from "react";
 import api from "../../services/api";
 
 import logo from "../../assets/logo.png";
 
-import { Container, Form } from "./styles";
+import {
+  Container,
+  Form
+} from "./styles";
 import CompareList from "../../components/CompareList";
 
 export default class Main extends Component {
@@ -28,21 +33,37 @@ export default class Main extends Component {
   };
 
   render() {
-    return (
-      <Container>
-        <img src={logo} alt="Github Compare" />
+    return ( <
+      Container >
+      <
+      img src = {
+        logo
+      }
+      alt = "Github Compare" / >
 
-        <Form onSubmit={this.handleAddRepositoy}>
-          <input
-            type="text"
-            placeholder="usuário/repositório"
-            value={this.state.repositoryInput}
-            onChange={e => this.setState({ repositoryInput: e.target.value })}
-          />
-          <button type="submit">OK</button>
-        </Form>
-        <CompareList repositories={this.state.repositories} />
-      </Container>
+      <
+      Form onSubmit = {
+        this.handleAddRepositoy
+      } >
+      <
+      input type = "text"
+      placeholder = "usuário/repositório"
+      value = {
+        this.state.repositoryInput
+      }
+      onChange = {
+        e => this.setState({
+          repositoryInput: e.target.value
+        })
+      }
+      /> <
+      button type = "submit" > OK < /button> < /
+      Form > <
+      CompareList repositories = {
+        this.state.repositories
+      }
+      /> < /
+      Container >
     );
   }
 }
